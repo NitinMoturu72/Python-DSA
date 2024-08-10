42f0388c596102e666908b9c7c85f61e2f2d1997
Snehita Moturu <nitinmoturu72@gmail.com>
2024-08-09 21:00:43 -0700

    09-Aug24 Lists

---
+++ lists.py
@@ -0,0 +1,15 @@
+integers = [1,2,3,4]
+stringlist = ['Milk', 'Cheese', 'Butter']
+mixlist = [1, 1.5, 'spam']
+nestedlist = [1,2,4,5,[1,2,3]]
+
+print(integers)
+print(stringlist)
+print(mixlist)
+print(nestedlist)
+
+print("Traversing the list")
+
+shoppinglist = ['Milk','Cheese','Butter']
+
+print(shoppinglist[0])
