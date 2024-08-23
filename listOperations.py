a640f6f0810f737577397e2d7e36ded587624f04
Snehita Moturu <nitinmoturu72@gmail.com>
2024-08-22 22:02:58 -0700

    22-Aug-24 Lists Operations

---
+++ listOperations.py
@@ -0,0 +1,19 @@
+myList = ['a','b','c','d','e','f']
+myList[0:2] = ['x','y']
+print(myList[:])
+
+ 
+myList.pop()
+print(myList)
+
+del myList[3]
+
+print(myList)
+
+
+myList.remove('e')
+
+print(myList)
+
+
+new_List = [10,20,30,40,50,60,70,80,90]
