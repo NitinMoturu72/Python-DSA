e904c2b6d029e7d1ede63c664c6e598b124d166a
Snehita Moturu <nitinmoturu72@gmail.com>
2024-08-22 20:45:25 -0700

    22-Aug-24 Lists

--- lists.py
+++ lists.py
@@ -12,4 +12,33 @@ print("Traversing the list")
 
 shoppinglist = ['Milk','Cheese','Butter']
 
-print(shoppinglist[0])
+for i in shoppinglist:
+    print(i)
+
+for i in range(len(shoppinglist)):
+    shoppinglist[i] = shoppinglist[i] + "+"
+    print(shoppinglist[i])
+
+print("Accessing elements")
+
+print(shoppinglist[-1])
+
+
+print("Update / Insert in list")
+
+myList = [1,2,3,4,5,6,7]
+print(myList)
+myList[2] = 33
+print(myList)
+
+print("Insertion of Elements")
+
+myList.insert(0,11)
+print(myList)
+
+
+myList.append(55)
+print(myList)
+
+myList.extend([1,2,3,4])
+print(myList)
