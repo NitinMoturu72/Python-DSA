0ec0212c6d78f5d8e64c893176f83c0b701af364
Snehita Moturu <nitinmoturu72@gmail.com>
2024-08-25 18:57:03 -0700

    25-Aug-24 Max Product

---
+++ maxProduct.py
@@ -0,0 +1,29 @@
+# Time complexity is O(n^2)
+def max_product1(arr):
+    product = 0
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            product = max(product, arr[i]*arr[j])
+    return product
+        
+print(max_product1([1,7,3,4,9,5]))
+
+# Time complexity is O(nlogn)
+def max_product2(arr):
+    arr.sort()
+    return arr[-1] * arr[-2]
+
+print(max_product2([1,7,3,4,9,5]))
+
+# Time complexity is O(n)
+def max_product3(arr):
+    max1 , max2 = 0 , 0 
+    for i in arr:
+        if i > max1:
+            max2 = max1
+            max1 = i
+        elif i > max2:
+            max2 = i
+    return max1 * max2
+
+print(max_product3([1,7,3,4,9,5]))
