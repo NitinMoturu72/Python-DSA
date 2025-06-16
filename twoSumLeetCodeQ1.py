fbc54ab7cffd4e2e62d6d05ca26acc223e82e8e5
Nitin Moturu <nitinmoturu72@gmail.com>
2025-06-15 18:57:12 -0700

    Brute force solution for Q1

--- twoSumLeetCodeQ1.py
+++ twoSumLeetCodeQ1.py
@@ -7,6 +7,22 @@
 # You can return the answer in any order.
 
 
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+# Brute force solution:
+# Iterate through the array with two nested loops to check every pair of numbers.
+# Return the indices of the two numbers that add up to the target.
+# Time complexity O(n^2) 
+
+
+
+
+
+
 def twoSum(nums, target):
         seen =  {}
         for i, value in enumerate(nums):
@@ -18,3 +34,11 @@ def twoSum(nums, target):
 
 
 print(twoSum([3,2,4], 6))
+
+# HashMap solution:
+# Use a dictionary to store the indices of the numbers as we iterate through the list.
+# For each number, check if the complement (target - number) exists in the dictionary.
+# If it does, return the indices of the current number and its complement.
+# Else add the current number and its index to the dictionary.
+# Time complexity O(n)  
+# Space complexity O(n) as we store the indices in a new dictionary.
