da31afa3d376ca72b334f666b1337d67f3a93f3a
Nitin Moturu <nitinmoturu72@gmail.com>
2025-06-18 22:22:06 -0700

    Valid Sudoku Q36

---
+++ ValidSudokuLeetCodeQ36.py
@@ -0,0 +1,51 @@
+# Valid Sudoku
+
+# You are given a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+# Each row must contain the digits 1-9 without duplicates.
+# Each column must contain the digits 1-9 without duplicates.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+# Return true if the Sudoku board is valid, otherwise return false
+
+# Note: A board does not need to be full or be solvable to be valid.
+
+def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(len(board)):
+            s = set()
+            for j in range(len(board)):
+                item = board[i][j]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+
+        for i in range(len(board)):
+            s = set()
+            for j in range(len(board)):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+
+        start = [(0,0), (0,3), (0,6),
+                (3,0), (3,3), (3,6),
+                (6,0), (6,3), (6,6)]
+
+        for i, j in start:
+            s = set()
+            for row in range(i, i+3):
+                for column in range(j, j+3):
+                    item = board[row][column]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+        return True
+
+# Brute force approach:
+# Row Check: For each row, we check if there are any duplicates of numbers 1-9 using a set. Return False if a duplicate is found. Else if the item is not a '.', we add it to the set.
+# Column Check: For each column, we do the same check for duplicates.
+# Box Check: For each of the nine 3x3 boxes, we check for duplicates in the same way as above.
+# Time Complexity: O(n^2), where n is the number of cells in the board (81 for a 9x9 board).
+# Space Complexity: O(1), since we are using a constant amount of extra space for the sets.
