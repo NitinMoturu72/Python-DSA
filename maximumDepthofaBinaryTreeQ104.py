8037afb47d8e85ce21a0d6605f200276154e7a6a
Nitin Moturu <nitinmoturu72@gmail.com>
2026-06-24 20:58:28 -0500

    Recursive DFS

--- maximumDepthofaBinaryTreeQ104.py
+++ maximumDepthofaBinaryTreeQ104.py
@@ -64,3 +64,25 @@ class Solution:
 # Time complexity: O(n), where n is the number of nodes in the tree. We visit each node once.
 # Space complexity: O(h), where h is the height of the tree. In the worst case, the stack will contain all the nodes in a single path from the root to a leaf node.
 
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+# Recursive Solution DFS
+# Base case: If the root is None, return 0.
+# Recursively call the maxDepth function on the left and right children of the root.
+# Return 1 plus the maximum of the depths of the left and right subtrees.
+# Time complexity: O(n), where n is the number of nodes in the tree. We visit each node once.
+# Space complexity: O(h), where h is the height of the tree. In the worst case, the recursion stack will contain all the nodes in a single path from the root to a leaf node.
