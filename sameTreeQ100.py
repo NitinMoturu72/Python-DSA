632128bfba8613ed59843b1ac7d9bb4912300def
Nitin Moturu <nitinmoturu72@gmail.com>
2026-06-25 21:16:40 -0500

    Recursive DFS

--- sameTreeQ100.py
+++ sameTreeQ100.py
@@ -70,3 +70,28 @@ class Solution:
 # If the loop completes without returning False, return True, indicating that the trees are the same.
 # Time Complexity: O(n), where n is the number of nodes in the trees. We visit each node once.
 # Space Complexity: O(h), where h is the height of the trees. In the worst case, the stack will contain all the nodes in a single path from the root to a leaf node.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val==q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        
+        else:
+            return False
+        
+
+# Recursive Solution DFS
+# Base case: If both trees are None, return True.
+# If both trees are not None and their values are equal, recursively check the left and right subtrees.
+# If either tree is None or their values are not equal, return False.
+# Time Complexity: O(n), where n is the number of nodes in the trees. We visit each node once.
+# Space Complexity: O(h), where h is the height of the trees. In the worst case, the recursion stack will contain all the nodes in a single path from the root to a leaf node.
